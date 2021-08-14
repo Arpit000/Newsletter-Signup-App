6065598bd60d6964f0df2db08e302fa1b1e93eab
arpit <arpitsaxena796@gmail.com>
2021-08-14 18:06:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const request = require("request")
+const https = require("https")
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+    
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    
+    const url = "https://us5.api.mailchimp.com/3.0/lists/3dffd5aff4";
+
+    const options = {
+      method: "POST",
+      auth: "arpit1:15e64298cb3b98971ddadd5586357c08-us5"
+    };
+
+    const request = https.request(url, options, function (response) {
+       
+        if (response.statusCode === 200)
+        {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server has been set to port 3000");
+});
+
+
+// api key
+// 15e64298cb3b98971ddadd5586357c08-us5
+
+// list id
+// 3dffd5aff4
